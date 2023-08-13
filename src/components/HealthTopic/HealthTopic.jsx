124ec17ec666ee1d4e4b71970306c2ae9048d928
Akshay Davbhat <72909367+Akshaydawbhat@users.noreply.github.com>
2023-08-13 15:45:11 +0530

    commit

--- src/components/HealthTopic/HealthTopic.jsx
+++ src/components/HealthTopic/HealthTopic.jsx
@@ -5,58 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 const HealthTopic = () => {
   return (
     <>
-      <div>
-        <Button as="a" variant="success">
-          Login
-        </Button>
-      </div>
-      <div>
-        <Card style={{ width: "18rem" }}>
-          <Card.Img variant="top" src="holder.js/100px180" />
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Button variant="primary">Go somewhere</Button>
-          </Card.Body>
-        </Card>
-      </div>
-      <div>
-        <Card>
-          <Card.Body>This is some text within a card body.</Card.Body>
-        </Card>
-      </div>
-      <div>
-        <Card style={{ width: "18rem" }}>
-          <Card.Body>
-            <Card.Title>Card Title</Card.Title>
-            <Card.Subtitle className="mb-2 text-muted">
-              Card Subtitle
-            </Card.Subtitle>
-            <Card.Text>
-              Some quick example text to build on the card title and make up the
-              bulk of the card's content.
-            </Card.Text>
-            <Card.Link href="#">Card Link</Card.Link>
-            <Card.Link href="#">Another Link</Card.Link>
-          </Card.Body>
-        </Card>
-        <Container>
-      <Row>
-        <Col xs={6} md={4}>
-          <Image src="holder.js/171x180" rounded />
-        </Col>
-        <Col xs={6} md={4}>
-          <Image src="holder.js/171x180" roundedCircle />
-        </Col>
-        <Col xs={6} md={4}>
-          <Image src="holder.js/171x180" thumbnail />
-        </Col>
-      </Row>
-    </Container>
-      </div>
+      <div>Health topics</div>
     </>
   );
 };
