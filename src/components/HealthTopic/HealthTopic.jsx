f039ff361784e4620530ac0f776a3909f098343c
Akshay Davbhat <72909367+Akshaydawbhat@users.noreply.github.com>
2023-08-13 15:19:28 +0530

    second commit

--- src/components/HealthTopic/HealthTopic.jsx
+++ src/components/HealthTopic/HealthTopic.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import { Button, Card, Col, Container, Image, Row } from "react-bootstrap";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const HealthTopic = () => {
   return (
-    <div>HealthTopic</div>
-  )
-}
+    <>
+      <div>
+        <Button as="a" variant="success">
+          Login
+        </Button>
+      </div>
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Img variant="top" src="holder.js/100px180" />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">Go somewhere</Button>
+          </Card.Body>
+        </Card>
+      </div>
+      <div>
+        <Card>
+          <Card.Body>This is some text within a card body.</Card.Body>
+        </Card>
+      </div>
+      <div>
+        <Card style={{ width: "18rem" }}>
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Subtitle className="mb-2 text-muted">
+              Card Subtitle
+            </Card.Subtitle>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
+            <Card.Link href="#">Another Link</Card.Link>
+          </Card.Body>
+        </Card>
+        <Container>
+      <Row>
+        <Col xs={6} md={4}>
+          <Image src="holder.js/171x180" rounded />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src="holder.js/171x180" roundedCircle />
+        </Col>
+        <Col xs={6} md={4}>
+          <Image src="holder.js/171x180" thumbnail />
+        </Col>
+      </Row>
+    </Container>
+      </div>
+    </>
+  );
+};
 
-export default HealthTopic
+export default HealthTopic;
